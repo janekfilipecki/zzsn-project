e976e2b9a201283e6f15c14b42d884d55650df4d
Jan Filipecki <plgfilipeckijan@login01.athena.cyfronet.pl>
2024-05-21 16:52:52 +0200

    fixed reqs

--- generate_sdxl.py
+++ generate_sdxl.py
@@ -4,7 +4,7 @@ import torch
 
 # Set the paths
 pretrained_model_name_or_path = "stabilityai/stable-diffusion-xl-base-1.0"
-lora_weights_path = "./models/dreambooth_lora_sdxl_dog6/pytorch_lora_weights.safetensors"
+lora_weights_path = "./models/dreambooth_lora_sdxl_dog6_2024-05-21_15-46-31/pytorch_lora_weights.safetensors"
 output_dir = "./output"
 
 # Load the base pipeline and apply the LoRA weights
