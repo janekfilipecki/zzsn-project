f54ee20a278c94613a068ba8e69ce14b0ad8234c
salbowic <s.albowicz@gmail.com>
2024-05-26 18:38:22 +0200

    modified:   conda_req.txt 	modified:   generate_sdxl.py 	new file:   generated/test_sdxl_hopefully_fixed.png 	new file:   generated/test_sdxl_hopefully_fixed2.png 	modified:   infere.slurm 	modified:   logs/dreambooth_lora_sdxl/train_dreambooth_lora_sdxl_724557_4294967294.out 	new file:   logs/dreambooth_lora_sdxl/train_dreambooth_lora_sdxl_724688_4294967294.out 	new file:   logs/dreambooth_lora_sdxl/train_dreambooth_lora_sdxl_727892_4294967294.out 	new file:   logs/dreambooth_lora_sdxl/train_dreambooth_lora_sdxl_727913_4294967294.out 	new file:   logs/dreambooth_lora_sdxl/train_dreambooth_lora_sdxl_727971_4294967294.out 	new file:   logs/dreambooth_lora_sdxl/train_dreambooth_lora_sdxl_728183_4294967294.out 	new file:   logs/dreambooth_lora_sdxl/train_dreambooth_lora_sdxl_729224_4294967294.out 	new file:   logs/dreambooth_lora_sdxl/train_dreambooth_lora_sdxl_lower_ppl_729350_4294967294.out 	new file:   logs/dreambooth_lora_sdxl/train_dreambooth_lora_sdxl_lower_ppl_729405_4294967294.out 	deleted:    logs/dreambooth_sdxl/train_dreambooth_sdxl_713533_4294967294.out 	deleted:    logs/dreambooth_sdxl/train_dreambooth_sdxl_713561_4294967294.out 	deleted:    logs/dreambooth_sdxl/train_dreambooth_sdxl_713620_4294967294.out 	deleted:    logs/dreambooth_sdxl/train_dreambooth_sdxl_713628_4294967294.out 	deleted:    logs/dreambooth_sdxl/train_dreambooth_sdxl_713635_4294967294.out 	deleted:    logs/dreambooth_sdxl/train_dreambooth_sdxl_713667_4294967294.out 	deleted:    logs/dreambooth_sdxl/train_dreambooth_sdxl_713678_4294967294.out 	deleted:    logs/dreambooth_sdxl/train_dreambooth_sdxl_713691_4294967294.out 	deleted:    logs/dreambooth_sdxl/train_dreambooth_sdxl_713704_4294967294.out 	deleted:    logs/dreambooth_sdxl/train_dreambooth_sdxl_713737_4294967294.out 	deleted:    logs/dreambooth_sdxl/train_dreambooth_sdxl_713757_4294967294.out 	deleted:    logs/dreambooth_sdxl/train_dreambooth_sdxl_713768_4294967294.out 	deleted:    logs/dreambooth_sdxl/train_dreambooth_sdxl_713776_4294967294.out 	deleted:    logs/dreambooth_sdxl/train_dreambooth_sdxl_713791_4294967294.out 	deleted:    logs/dreambooth_sdxl/train_dreambooth_sdxl_713792_4294967294.out 	deleted:    logs/generate_713800_4294967294.log 	deleted:    logs/generate_713802_4294967294.log 	deleted:    logs/generate_713813_4294967294.log 	deleted:    logs/generate_713819_4294967294.log 	deleted:    logs/generate_713905_4294967294.log 	deleted:    logs/generate_713908_4294967294.log 	deleted:    logs/generate_713927_4294967294.log 	deleted:    logs/generate_713928_4294967294.log 	deleted:    logs/generate_713930_4294967294.log 	new file:   logs/generate_726391_4294967294.log 	new file:   logs/generate_726401_4294967294.log 	new file:   logs/generate_726404_4294967294.log 	new file:   logs/generate_727898_4294967294.log 	new file:   logs/generate_727953_4294967294.log 	new file:   logs/generate_728023_4294967294.log 	new file:   logs/generate_728293_4294967294.log 	new file:   logs/generate_729335_4294967294.log 	new file:   logs/generate_729401_4294967294.log 	new file:   logs/generate_729403_4294967294.log 	new file:   logs/generate_729668_4294967294.log 	modified:   pip_req.txt 	modified:   requirements.txt 	deleted:    test_sdxl_dog.png 	deleted:    test_sdxl_dog_simple.png 	modified:   train_dreambooth_lora_sdxl.slurm 	modified:   train_dreambooth_sdxl.slurm 	new file:   train_dreambooth_ziplora_sdxl.slurm 	new file:   train_loras_for_subject_or_style.slurm

--- generate_sdxl.py
+++ generate_sdxl.py
@@ -4,7 +4,7 @@ import torch
 
 # Set the paths
 pretrained_model_name_or_path = "stabilityai/stable-diffusion-xl-base-1.0"
-lora_weights_path = "./models/dreambooth_lora_sdxl_dog6/pytorch_lora_weights.safetensors"
+lora_weights_path = "./models/dreambooth_lora_sdxl_dog3_2024-05-24_23-43-20/pytorch_lora_weights.safetensors"
 output_dir = "./output"
 
 # Load the base pipeline and apply the LoRA weights
@@ -12,17 +12,9 @@ pipe = DiffusionPipeline.from_pretrained(pretrained_model_name_or_path, torch_dt
 pipe = pipe.to("cuda")
 pipe.load_lora_weights(lora_weights_path)
 
-# Load the refiner
-refiner = StableDiffusionXLImg2ImgPipeline.from_pretrained(
-    "stabilityai/stable-diffusion-xl-refiner-1.0", torch_dtype=torch.float16, use_safetensors=True, variant="fp16"
-)
-refiner.to("cuda")
-
 # Define the prompt and the random generator
-prompt = "a sks dog"
-generator = torch.Generator("cuda").manual_seed(0)
+prompt = "a photo of sks dog"
 
-# Perform inference with the base pipeline and refine the output
-image = pipe(prompt=prompt, output_type="latent", generator=generator).images[0]
-refined_image = refiner(prompt=prompt, image=image[None, :], generator=generator).images[0]
-refined_image.save("test_sdxl_dog_simple.png")
+# Perform inference with the base pipeline
+image = pipe(prompt=prompt, num_inference_steps=25).images[0]
+image.save("generated/test_sdxl_hopefully_fixed2.png")
