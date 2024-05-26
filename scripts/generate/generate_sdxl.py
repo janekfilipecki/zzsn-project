0daced653bd9a907201c9495768383fb2468c1bb
Jan Filipecki <plgfilipeckijan@login01.athena.cyfronet.pl>
2024-05-26 18:22:06 +0200

    added lpips and cleaned up repo

--- scripts/generate/generate_sdxl.py
+++ scripts/generate/generate_sdxl.py
@@ -4,8 +4,8 @@ import torch
 
 # Set the paths
 pretrained_model_name_or_path = "stabilityai/stable-diffusion-xl-base-1.0"
-lora_weights_path = "./models/dreambooth_lora_sdxl_dog6_2024-05-21_15-46-31/pytorch_lora_weights.safetensors"
-output_dir = "./output"
+lora_weights_path = "../../models/dreambooth_lora_sdxl_dog6_2024-05-21_15-46-31/pytorch_lora_weights.safetensors"
+output_dir = "../../generated"
 
 # Load the base pipeline and apply the LoRA weights
 pipe = DiffusionPipeline.from_pretrained(pretrained_model_name_or_path, torch_dtype=torch.float16)
