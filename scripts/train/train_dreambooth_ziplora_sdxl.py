a4f0f86c86c4a7d22d0dffb509b994852a8aaa20
salbowic <s.albowicz@gmail.com>
2024-05-27 02:49:01 +0200

    modified:   conda_req.txt 	new file:   generated/test_style1_dog.png 	new file:   generated/test_subject_dog3.png 	new file:   logs/dreambooth_lora_sdxl/train_lora_for_style_lower_ppl_731315_4294967294.out 	new file:   logs/dreambooth_lora_sdxl/train_lora_for_style_lower_ppl_731599_4294967294.out 	new file:   logs/dreambooth_lora_sdxl/train_lora_for_subject_lower_ppl_731463_4294967294.out 	new file:   logs/dreambooth_lora_sdxl/train_lora_for_subject_lower_ppl_731566_4294967294.out 	new file:   logs/dreambooth_lora_sdxl/train_loras_for_ziplora_lower_ppl_731291_4294967294.out 	new file:   logs/dreambooth_lora_sdxl/train_ziplora_731347_4294967294.out 	new file:   logs/dreambooth_lora_sdxl/train_ziplora_731348_4294967294.out 	new file:   logs/dreambooth_lora_sdxl/train_ziplora_731349_4294967294.out 	new file:   logs/dreambooth_lora_sdxl/train_ziplora_731352_4294967294.out 	new file:   logs/dreambooth_lora_sdxl/train_ziplora_731393_4294967294.out 	new file:   logs/dreambooth_lora_sdxl/train_ziplora_731414_4294967294.out 	new file:   logs/dreambooth_lora_sdxl/train_ziplora_731415_4294967294.out 	new file:   logs/dreambooth_lora_sdxl/train_ziplora_731416_4294967294.out 	new file:   logs/dreambooth_lora_sdxl/train_ziplora_731439_4294967294.out 	new file:   logs/dreambooth_lora_sdxl/train_ziplora_731454_4294967294.out 	new file:   logs/dreambooth_lora_sdxl/train_ziplora_731545_4294967294.out 	new file:   logs/dreambooth_lora_sdxl/train_ziplora_731641_4294967294.out 	deleted:    logs/generate_726391_4294967294.log 	deleted:    logs/generate_726401_4294967294.log 	deleted:    logs/generate_726404_4294967294.log 	deleted:    logs/generate_727898_4294967294.log 	deleted:    logs/generate_727953_4294967294.log 	deleted:    logs/generate_728023_4294967294.log 	deleted:    logs/generate_728293_4294967294.log 	deleted:    logs/generate_729335_4294967294.log 	deleted:    logs/generate_729401_4294967294.log 	deleted:    logs/generate_729403_4294967294.log 	deleted:    logs/generate_729668_4294967294.log 	new file:   logs/generation_logs/generate_731341_4294967294.log 	new file:   logs/generation_logs/generate_731345_4294967294.log 	new file:   logs/generation_logs/generate_731535_4294967294.log 	modified:   pip_req.txt 	new file:   requirements_ziplora.txt 	modified:   scripts/generate/generate_sdxl.py 	modified:   scripts/metrics/calculate_clip_i.slurm 	modified:   scripts/metrics/calculate_clip_t.slurm 	modified:   scripts/metrics/calculate_dino.slurm 	modified:   scripts/metrics/calculate_lpips.slurm 	new file:   scripts/train/__pycache__/ziplora_utils.cpython-39.pyc 	new file:   scripts/train/train_dreambooth_lora_for_style.slurm 	new file:   scripts/train/train_dreambooth_lora_for_subject.slurm 	modified:   scripts/train/train_dreambooth_lora_sdxl.slurm 	modified:   scripts/train/train_dreambooth_ziplora_sdxl.py 	new file:   scripts/train/train_dreambooth_ziplora_sdxl.slurm 	new file:   scripts/train/train_ziplora.slurm 	new file:   scripts/train/ziplora_utils.py 	renamed:    train_dreambooth_ziplora_sdxl.slurm -> test.slurm 	deleted:    train_loras_for_subject_or_style.slurm 	new file:   ziplora_pytorch/__init__.py 	new file:   ziplora_pytorch/ziplora.py

--- scripts/train/train_dreambooth_ziplora_sdxl.py
+++ scripts/train/train_dreambooth_ziplora_sdxl.py
@@ -59,8 +59,8 @@ from diffusers.optimization import get_scheduler
 from diffusers.training_utils import compute_snr, unet_lora_state_dict
 from diffusers.utils import check_min_version, is_wandb_available
 from diffusers.utils.import_utils import is_xformers_available
-from ziplora_pytorch import ZipLoRALinearLayer
-from ziplora_pytorch.utils import (
+from ziplora_utils import ZipLoRALinearLayer
+from ziplora_utils import (
     get_lora_weights,
     merge_lora_weights,
     initialize_ziplora_layer,
