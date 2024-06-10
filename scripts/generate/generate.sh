23c72ffb6920ec8972a25d82eeed91b40d066ef5
Jan Filipecki <plgfilipeckijan@login01.athena.cyfronet.pl>
2024-06-10 17:39:40 +0200

    added better generation scripts

---
+++ scripts/generate/generate.sh
@@ -0,0 +1,6 @@
+#! /bin/bash
+sbatch scripts/generate/infere.slurm scripts/generate/generate_sdxl.py \
+    --weights_path="models/dreambooth_lora_sdxl_berry_bowl_2024-05-27_18-44-03/pytorch_lora_weights.safetensors" \
+    --output_dir="generated/berry_bowl" \
+    --prompt="a berry_bowl bowl in front of a skyscraper" \
+    --num_inference_steps=100
--- scripts/generate/generate_sdxl.py
+++ scripts/generate/generate_sdxl.py
@@ -1,20 +1,28 @@
-from huggingface_hub import snapshot_download
 from diffusers import DiffusionPipeline, StableDiffusionXLImg2ImgPipeline
-import torch
+import torch, argparse
+from datetime import datetime
+
+parser = argparse.ArgumentParser()
+parser.add_argument("--weights_path", required=True)
+parser.add_argument("--output_dir", required=True)
+parser.add_argument("--prompt", required=True)
+parser.add_argument("--num_inference_steps", type=int, required=True)
+
+args = parser.parse_args()
 
 # Set the paths
 pretrained_model_name_or_path = "stabilityai/stable-diffusion-xl-base-1.0"
-lora_weights_path = "../../models/dreambooth_lora_sdxl_dog6_2024-05-21_15-46-31/pytorch_lora_weights.safetensors"
-output_dir = "../../generated"
+lora_weights_path = args.weights_path
+output_dir = args.output_dir
+prompt = args.prompt
+num_inference_steps = args.num_inference_steps
+
 
 # Load the base pipeline and apply the LoRA weights
 pipe = DiffusionPipeline.from_pretrained(pretrained_model_name_or_path, torch_dtype=torch.float16)
 pipe = pipe.to("cuda")
 pipe.load_lora_weights(lora_weights_path)
 
-# Define the prompt and the random generator
-prompt = "sks dog in a bucket"
-
 # Perform inference with the base pipeline
-image = pipe(prompt=prompt, num_inference_steps=25).images[0]
-image.save("generated/test_sdxl_another.png")
+image = pipe(prompt=prompt, num_inference_steps=num_inference_steps).images[0]
+image.save(output_dir + "/" + prompt.replace(" ", "_") + "_" + str(datetime.now()) + ".png")
