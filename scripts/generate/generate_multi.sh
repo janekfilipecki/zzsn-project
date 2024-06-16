e7ddefa6e8c9274c996bda488561dba6da0350ae
Jan Filipecki <plgfilipeckijan@login01.athena.cyfronet.pl>
2024-06-16 17:43:47 +0200

    added most recent logs

--- scripts/generate/generate_multi.sh
+++ scripts/generate/generate_multi.sh
@@ -1,7 +1,7 @@
 #! /bin/bash
 sbatch scripts/generate/infere.slurm scripts/generate/generate_sdxl.py \
-    --weights_path="models/dreambooth_lora_sdxl_dog6_2024-06-13_10-31-11/pytorch_lora_weights.safetensors" \
-    --output_dir="generated/dog6_maybe_fixed" \
-    --prompt_input="prompts/dog6_prompts.txt" \
+    --weights_path="models/dreambooth_lora_sdxl_can_2024-06-15_18-29-12/pytorch_lora_weights.safetensors" \
+    --output_dir="generated/can_3000" \
+    --prompt_input="prompts/can_prompts.txt" \
     --num_generations=1 \
     --num_inference_steps=30
--- scripts/generate/generate_single.sh
+++ scripts/generate/generate_single.sh
@@ -1,7 +1,7 @@
 #! /bin/bash
 sbatch scripts/generate/infere.slurm scripts/generate/generate_sdxl.py \
-    --weights_path="models/dreambooth_lora_sdxl_no_ppl_dog6_2024-06-13_07-39-19/pytorch_lora_weights.safetensors" \
-    --output_dir="generated/dog6" \
-    --prompt_input="A photo of sks dog in a bucket" \
+    --weights_path="models/dreambooth_lora_sdxl_no_ppl_can_2024-06-15_18-20-38/pytorch_lora_weights.safetensors" \
+    --output_dir="generated/can_no_ppl_no_target" \
+    --prompt_input="a photo of a can" \
     --num_generations=5 \
     --num_inference_steps=50
